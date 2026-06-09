20662ccb6f0d0650562daed6cb67541e0e5e6cfa
jamsaeed <jameela.mohamed.saeed@gmail.com>
2026-06-09 17:46:53 +0300

    fix pop up + css+ add quit

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let score = 0
 let timer = 60
+let isChecking = false
+let startGame = false
+let timeDown
 let rePlay = document.querySelector(".play-button")
-console.log(rePlay)
-
 let gameContainer = document.querySelector(".game-container")
 let timeDisplay = document.querySelector(".counting-time")
 let scoreDisplay = document.querySelector(".cards-score")
-
+let matchedCards = []
+//popup
+let losePopUp = document.querySelector(".pop-container")
+let winPopUp = document.querySelector(".pop-container2")
+let losingScore = document.querySelector("#LosingMessage")
+let winScore = document.querySelector("#message")
+let playAgain = document.querySelector("#playAgain")
+let losePlayAgain = document.querySelector("#loseplayAgain")
 
 const memoryCard = [
   "./images/image0.jpeg",
@@ -25,7 +33,7 @@ const memoryCard = [
 
 let cards = []
 let flippedCards = []
-const shuffleCards = memoryCard.sort(() => Math.random() - 0.5)
+let shuffleCards = memoryCard.sort(() => Math.random() - 0.5)
 
 for (let i = 0; i < shuffleCards.length; i++) {
   const container = document.createElement("div")
@@ -37,22 +45,59 @@ for (let i = 0; i < shuffleCards.length; i++) {
   cards.push(img)
 
   cards[i].addEventListener("click", () => {
+    //handle checking
+    if (isChecking === true) {
+      return // stop wait until checing is done
+    }
+
+    if (matchedCards.includes(img)) {
+      return
+    }
+    // guard condition other wise get10 points for clicking the same card twice
+    if (flippedCards.includes(img)) {
+      return //stops and doesn't let clicking the same card twice
+    }
+
+    //Don’t allow flipping another card if two are already flipped.
+    if (flippedCards.length >= 2) {
+      return //stop should not flip more than two cards
+    }
+
+    if (!startGame) {
+      startGame = true
+      timeDown = setInterval(downCount, 1000)
+    }
+
     img.setAttribute("src", `${shuffleCards[i]}`)
 
     console.log("clicked")
     flippedCards.push(img)
 
     if (flippedCards.length === 2) {
-      console.log(flippedCards)
+      isChecking = true // to check match
+      //console.log(flippedCards)
       if (flippedCards[0].src === flippedCards[1].src) {
         score += 10
         scoreDisplay.innerText = score
+        matchedCards.push(flippedCards[0])
+        matchedCards.push(flippedCards[1])
         flippedCards = []
+        isChecking = false // done checking match
+        if (score === 50) {
+          clearInterval(timeDown)
+          isChecking = true
+          setTimeout(() => {
+            winning()
+            // alert(`you woon! score ${score}`)
+          }, 100)
+        }
       } else {
         const [card0, card1] = flippedCards
+        //They don't match - flip back after 500ms
         setTimeout(() => {
           card0.setAttribute("src", "./images/backimage.jpeg")
           card1.setAttribute("src", "./images/backimage.jpeg")
+          isChecking = false
         }, 500)
         flippedCards = []
       }
@@ -62,21 +107,28 @@ for (let i = 0; i < shuffleCards.length; i++) {
 }
 
 const downCount = () => {
+  timeDisplay.innerText = timer
+
   if (timer === 0) {
-    // console.log("!")
+    clearInterval(timeDown)
+    isChecking = true
+    setTimeout(() => {
+      // alert('game over')
+      losing()
+    }, 100)
   } else {
-    timeDisplay.innerText = timer
     timer--
   }
 }
-let timeDown = setInterval(downCount, 1000)
+
+//  timeDown = setInterval(downCount, 1000) this will make the time start even before playing
 
 const reSet = () => {
   clearInterval(timeDown)
-  setInterval(downCount, 1000)
   timer = 60
+  timeDisplay.innerText = timer
+  timeDown = setInterval(downCount, 1000)
 }
-//
 
 const reLoad = () => {
   let button = document.querySelector(".play-button")
@@ -85,9 +137,59 @@ const reLoad = () => {
   }
 }
 
+document.querySelector("#main-menu").addEventListener("click", () => {
+  document.location.href = "index.html"
+})
+
 rePlay.addEventListener("click", () => {
   reSet()
   score = 0
   scoreDisplay.innerText = score
+
+  flippedCards = []
+  matchedCards = []
+  isChecking = false
+  startGame = false
+
+  shuffleCards = memoryCard.sort(() => Math.random() - 0.5)
+
+  cards.forEach((card, index) => {
+    card.setAttribute("src", "./images/backimage.jpeg")
+  })
 })
 
+// handle pop up
+
+// handles winning pop up
+const winning = () => {
+  winPopUp.classList.add("show")
+  winScore.innerText = `Your Score : ${score}`
+}
+
+// handles losing pop up
+const losing = () => {
+  losePopUp.classList.add("show")
+  losingScore.innerText = `Your Score : ${score}`
+}
+
+const hidePopUp = () => {
+  winPopUp.classList.remove("show")
+  losePopUp.classList.remove("show")
+}
+
+playAgain.addEventListener("click", () => {
+  hidePopUp()
+  rePlay.click()
+})
+
+losePlayAgain.addEventListener("click", () => {
+  hidePopUp()
+  rePlay.click()
+})
+
+document.querySelector("#quit").addEventListener("click", () => {
+  window.location.href = "index.html"
+})
+document.querySelector("#losingquit").addEventListener("click", () => {
+  window.location.href = "index.html"
+})
